f50566d16f41012886ea8961e5ebac0c831f6cd9
Joshua Koester <joshuakoester@gmail.com>
2024-07-04 19:11:09 -0500

    jobs can be added to schedulier

--- PingBuddy/ScheduleJobForm.cs
+++ PingBuddy/ScheduleJobForm.cs
@@ -7,12 +7,17 @@ namespace PingBuddy
     public partial class ScheduleJobForm : Form
     {
         private List<PingJob> availableJobs;
+        private List<ScheduledJob> scheduledJobs;
 
         public ScheduleJobForm(List<PingJob> jobs)
         {
             InitializeComponent();
             availableJobs = jobs;
+            scheduledJobs = new List<ScheduledJob>();
             PopulateAvailableJobs();
+
+            // Wire up the Schedule button click event
+            scheduleButton.Click += ScheduleButton_Click;
         }
 
         private void PopulateAvailableJobs()
@@ -26,5 +31,51 @@ namespace PingBuddy
                 availableJobsListView.Items.Add(item);
             }
         }
+
+        private void ScheduleButton_Click(object sender, EventArgs e)
+        {
+            if (availableJobsListView.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Please select a job to schedule.", "No Job Selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            PingJob selectedJob = (PingJob)availableJobsListView.SelectedItems[0].Tag;
+            DateTime startTime = startDateTimePicker.Value;
+            TimeSpan duration = TimeSpan.FromMinutes((double)durationNumericUpDown.Value);
+
+            ScheduledJob newScheduledJob = new ScheduledJob
+            {
+                Job = selectedJob,
+                StartTime = startTime,
+                Duration = duration,
+                Status = "Pending"
+            };
+
+            scheduledJobs.Add(newScheduledJob);
+            UpdateScheduledJobsListView();
+        }
+
+        private void UpdateScheduledJobsListView()
+        {
+            scheduledJobsListView.Items.Clear();
+            foreach (var scheduledJob in scheduledJobs)
+            {
+                ListViewItem item = new ListViewItem(scheduledJob.Job.Name);
+                item.SubItems.Add(scheduledJob.StartTime.ToString("g"));
+                item.SubItems.Add(scheduledJob.Duration.TotalMinutes.ToString() + " min");
+                item.SubItems.Add(scheduledJob.Status);
+                item.Tag = scheduledJob;
+                scheduledJobsListView.Items.Add(item);
+            }
+        }
+    }
+
+    public class ScheduledJob
+    {
+        public PingJob Job { get; set; }
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Status { get; set; }
     }
 }
